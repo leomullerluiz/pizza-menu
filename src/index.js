248226567496f3c49db54956c954c91105616573
Leonan Müller <leomullerluiz@gmail.com>
2024-10-05 20:35:45 -0300

    add tailwind

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@
 // -----------------------------------------------------------------------------
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 function App(){
 
     return(
         <div>
-            <h1>Pizza Menu</h1>
+            <h1 className='text-3xl font-bold underline'>Pizza Menu</h1>
         </div>
-    )
+    );
+
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App/></React.StrictMode>);
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
