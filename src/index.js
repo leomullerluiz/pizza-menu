974ed447718499b683653b13506a1b6c22851bdb
Leonan Müller <leomullerluiz@gmail.com>
2024-10-05 20:16:41 -0300

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
+// Objetivo do projeto: 
+// Aplicar os conceitos básicos de React: components, props, jsx.
+// Criar componentes reutilizaveis.
+// Listas de renderização
+// Renderização condicional
+// -----------------------------------------------------------------------------
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+function App(){
+
+    return(
+        <div>
+            <h1>Pizza Menu</h1>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><App/></React.StrictMode>);
