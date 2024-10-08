5f21f9ff98c4804a6035f8dc380df32669f5ea57
Leonan Müller <leomullerluiz@gmail.com>
2024-10-08 19:32:19 -0300

    add map

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Pizza from './Pizza.jsx';
+import { pizzaData } from '../data.js';
 
 export default function Menu() {
+    
     return (
         <div className='flex flex-col space-y-4 min-h-screen'>
-            <Pizza data={0}/>
-            <Pizza data={1}/>
-            <Pizza data={2}/>
-            <Pizza data={3}/>
-            <Pizza data={4}/>
-            <Pizza data={5}/>
+            <div>
+                {
+                    pizzaData.map((pizza) => (
+                        <Pizza pizzaObj={pizza} />
+                    ))
+                }
+            </div>
         </div>
     );
 }
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { pizzaData } from '../data.js';
 
 export default function Pizza(props){
 
     return (
-        <div className="p-6 w-80 mx-auto bg-amber-200 rounded-xl shadow-lg flex items-center space-x-4">
+        
+        <div className="p-6 mt-3 w-80 mx-auto bg-amber-200 rounded-xl shadow-lg flex items-center space-x-4 ">
+            {console.log(props)}
             <div className="shrink-0">
-                <img className="size-12 rounded-xl" src={pizzaData[props.data].photoName} alt={pizzaData[props.data].name} />
+                <img className="size-12 rounded-xl" src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
             </div>
             <div>
-                <div className="text-xl font-medium text-black">{pizzaData[props.data].name}</div>
-                <p className="text-slate-500">{pizzaData[props.data].ingredients}</p>
+                <div className="text-xl font-medium text-black">{props.pizzaObj.name}</div>
+                <p className="text-slate-500">{props.pizzaObj.ingredients} - <span className='text-slate-950'>${props.pizzaObj.price}</span></p>
             </div>
         </div>
     );
